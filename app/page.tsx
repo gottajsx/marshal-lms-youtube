6419cd63890043612518c687a256d936cff494ed
gottajsx <gottajsx@gmail.com>
2025-07-10 16:33:18 +0200

    Create Login UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { ThemeToggle } from "@/components/ui/themeToggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1 className="text-2xl font-bold text-red-500">Hello world</h1>
+
+      <ThemeToggle />
     </div>
   );
 }
