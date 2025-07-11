325949e9cdb25787dcb07dbf6cd15a36065ba016
gottajsx <gottajsx@gmail.com>
2025-07-11 13:18:33 +0200

    Implement sign-out for github auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,40 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/themeToggle";
-import Image from "next/image";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export default function Home() {
+  const router = useRouter();
+  const { data: session } = authClient.useSession();
+
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/"); // redirect to login page
+          toast.success("Sign-Out Successfully");
+        },
+      },
+    });
+  }
+
   return (
-    <div>
+    <div className="p-24">
       <h1 className="text-2xl font-bold text-red-500">Hello world</h1>
 
       <ThemeToggle />
+      
+      {session ? (
+        <div>
+          <p>{session.user.name}</p>
+          <Button onClick={signOut}>Logout</Button>
+        </div>
+      ) : (
+        <Button>Login</Button>
+      )}
     </div>
   );
 }
