eef6fb433d0dc869255bc2af17419098b04fec3c
gottajsx <gottajsx@gmail.com>
2025-07-22 13:28:37 +0200

    Create mutation layer

---
+++ app/admin/courses/create/actions.ts
@@ -0,0 +1,43 @@
+"use server";
+
+import { auth } from "@/lib/auth";
+import { prisma } from "@/lib/db";
+import { ApiResponse } from "@/lib/types";
+import { courseSchema, CourseSchemaType } from "@/lib/zodSchemas";
+import { headers } from "next/headers";
+
+export async function CreateCourse(values: CourseSchemaType):Promise<ApiResponse> {
+
+    try {
+        const session = await auth.api.getSession({
+            headers: await headers()
+        })
+        
+        const validation = courseSchema.safeParse(values);
+
+        if (!validation.success) {
+            return {
+                status: 'error',
+                message: 'Invalid Fprm Data',
+            }
+        }
+
+        const data = await prisma.course.create({
+            data: {
+                ...validation.data,
+                userId: session?.user.id as string,
+            },
+        });
+
+        return {
+            status: 'success',
+            message: 'Course created successfully',
+        }
+    } catch {
+        return {
+            status: 'error',
+            message: 'Faéiled to create course',
+        }
+    }
+
+}
--- app/admin/courses/create/page.tsx
+++ app/admin/courses/create/page.tsx
@@ -3,7 +3,7 @@
 import { Button, buttonVariants } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { courseCategories, courseLevels, courseSchema, CourseSchemaType, courseStatus } from "@/lib/zodSchemas";
-import { ArrowLeft, PlusIcon, SparkleIcon } from "lucide-react";
+import { ArrowLeft, Loader2, PlusIcon, SparkleIcon } from "lucide-react";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -15,8 +15,16 @@ import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { RichTextEditor } from "@/components/rich-text-editor/Editor";
 import { Uploader } from "@/components/file-uploader/Uploader";
+import { useTransition } from "react";
+import { tryCatch } from "@/hooks/try-catch";
+import { CreateCourse } from "./actions";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export default function CourseCreationPage() {
+const [pending, startTransition] = useTransition();
+const router = useRouter();
+
     // 1. Define your form
     const form = useForm<CourseSchemaType>({
         resolver: zodResolver(courseSchema),
@@ -36,9 +44,23 @@ export default function CourseCreationPage() {
 
     // 2. Define a submit handler.
     function onSubmit(values: CourseSchemaType) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+        startTransition(async () => {
+            const { data: result, error } = await tryCatch(CreateCourse(values));
+
+            if(error) {
+                toast.error("An unexpected error occured. Please try again.");
+                return;
+            }
+
+            if(result.status === 'success') {
+                toast.success(result.message);
+                form.reset();
+                router.push("/admin/courses")
+
+            } else if (result.status === 'error') {
+                toast.error(result.message);
+            }
+        })
     }
 
     return(
@@ -275,8 +297,18 @@ export default function CourseCreationPage() {
                                 )}
                             />
 
-                            <Button>
-                                Create Course <PlusIcon className="ml-1" size={16} />
+                            <Button type="submit" disabled={pending}>
+                                {pending ? (
+                                    <>
+                                        Creating...
+                                        <Loader2 className="animate-spin ml-1" />
+                                    </>
+                                ) : (
+                                    <>
+                                        Create Course <PlusIcon className="ml-1" size={16} />
+                                    </>
+                                )}
+                               
                             </Button>
                         </form>
                     </Form>
