47dc809da87f775b370de698a19276b087536cbb
gottajsx <gottajsx@gmail.com>
2025-07-23 12:29:13 +0200

    Display Courses

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -1,7 +1,10 @@
+import { adminGetCourses } from "@/app/data/admin/admin-get-sourses";
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
+import { AdminCourseCard } from "./_components/AdminCourseCard";
 
-export default function CoursesPage() {
+export default async function CoursesPage() {
+    const data = await adminGetCourses();
     return(
         <>
             <div className="flex items-center justify-between">
@@ -10,8 +13,10 @@ export default function CoursesPage() {
                 <Link className={buttonVariants()} href="/admin/courses/create">Create Course</Link>
             </div>
 
-            <div>
-                <h1>Here you will see all of the courses</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-7">
+                {data.map((course) => (
+                    <AdminCourseCard key={course.id} data={course} />
+                ))}
             </div>
         </>
     )
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "marshal-lms-yt-video-subscribe-gottajsx.fly.storage.tigris.dev",
+        port: "",
+        protocol: "https",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
