1465134a8b6b7a4ead8f39263a184ed02791ec88
gottajsx <gottajsx@gmail.com>
2025-07-24 14:03:22 +0200

    Create Course Structure (Drag and Drop)

--- app/admin/courses/[courseId]/edit/page.tsx
+++ app/admin/courses/[courseId]/edit/page.tsx
@@ -2,6 +2,7 @@ import { adminGetCourse } from "@/app/data/admin/admin-get-course";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { EditCourseForm } from "./_components/EditCourseForm";
+import { CourseStructure } from "./_components/CourseStructure";
 
 type Params = Promise<{ courseId: string}>
 
@@ -27,6 +28,17 @@ export default async function EditRoute({ params } : { params: Params }) {
                         </CardHeader>
                         <CardContent>
                             <EditCourseForm data={data}/>
+                        </CardContent>
+                    </Card>
+                </TabsContent>
+                <TabsContent value="course-structure">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Course Structure</CardTitle>
+                            <CardDescription>Here you can update your Course Structure</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <CourseStructure data={data} />
                         </CardContent>
                     </Card>
                 </TabsContent>
